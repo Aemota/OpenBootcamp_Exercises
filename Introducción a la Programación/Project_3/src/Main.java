c5625a71aa14bd70badf602b5f1eadc416dc18d3
Alba <104780279+Aemota@users.noreply.github.com>
2023-01-09 16:32:45 +0100

    Add files via upload

---
+++ Introducción a la Programación/Project_3/src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Persona Maria = new Persona();
+        Maria.setNombre("María");
+        Maria.setEdad(30);
+        Maria.setTelefono(123456789);
+        System.out.print("Mi nombre es: " + Maria.getNombre() + ". \n" + "Tengo " + Maria.getEdad() + " años. \n" + "Y mi teléfono es el: " + Maria.getTelefono());
+    }
+}
+
+class Persona {
+    private int edad;
+    private String nombre;
+    private int telefono;
+
+    // SETTERS
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public void setTelefono(int telefono){
+        this.telefono = telefono;
+    }
+
+
+    // GETTERS
+
+    public int getEdad(){
+        return this.edad;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+    public int getTelefono(){
+        return this.telefono;
+    }
+
+}
